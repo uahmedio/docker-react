27f5cb47adc734cefd05cbfcf4914a2b47ff5e8f
Usama Ahmed <usama009lio@gmail.com>
2019-08-10 23:05:53 +0200

    changed text feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>This is working now </p>
+				<p>AWS</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
